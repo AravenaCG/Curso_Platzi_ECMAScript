35abb8e88562542a6680522ea3376157bdddbd16
caravena <caravena@exisoft.com.ar>
2023-01-05 18:50:09 -0300

    Se agregan las otras actualizaciones de ES6

--- src/ES6/07-clases.js
+++ src/ES6/07-clases.js
@@ -1,5 +1,71 @@
+//declaramos la clase
 class User{
 
+//constructor
+constructor(){
+    console.log('Nuevo usuario');
+}
+    //metodos
+greeting(){
+    return 'Hello';
+}
 };
-
+//instancia de una clase
 const newUsert = new User();
+console.log(newUsert.greeting());
+
+const secondUser = new User();
+console.log(secondUser.greeting());
+ 
+const cristian = new User();
+
+//this
+
+class User{
+    constructor(name){
+        this.name= name;
+    }
+    //metodos
+    speak(){
+        return 'Hello';
+    }
+
+    greeting(){
+        return `${this.speak()} ${this.name}`
+    }
+}
+
+const thirdUser = new User('Cristian');
+console.log(thirdUser.greeting());
+
+//getters and setters
+
+class User{
+
+    constructor(name,age){
+        this.name= name;
+        this.age=age;
+    }
+
+    //metodos
+    speak(){
+        return 'Hello';
+    }
+
+    greeting(){
+        return `${this.speak} ${this.name}`;
+    }
+
+    get uAge(){
+        return this.age;
+    }
+
+    set uAge(nAge){
+        this.age=nAge;
+    }
+
+
+}
+const bebeloper = new User('Cristian2',34);
+console.log(bebeloper.uAge);
+console.log(bebeloper.uAge = 35);
---
+++ src/ES6/10-set-add.js
@@ -0,0 +1,5 @@
+const list = new Set();
+list.add(1);
+list.add(2).add(3);
+
+console.log(list);
