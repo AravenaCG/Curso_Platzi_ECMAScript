d9ac7162e73beaf8bd19b4675a8b82f83ae173b6
caravena <caravena@exisoft.com.ar>
2023-01-06 21:57:04 -0300

    Se termina de subir de ECMAScript

--- src/ES13/products.js
+++ src/ES13/products.js
@@ -1,2 +1,10 @@
 import fetch from "node-fetch";
+const API = 'https://api.escuelajs.co/';
+const GETProducts = 'api/v1/products'
+console.log(`${API}${GETProducts}`);
 
+
+const response = await fetch(`${API}${GETProducts}`);
+const products = await response.json();
+
+export { products };
